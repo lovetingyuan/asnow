97d953ba17db99efc4d6af5f8a94d27be38b8a33
Baitu Huang <baitu.huang@shopeemobile.com>
2018-07-11 20:00:01 +0800

    remove comment

--- runtime/render/updateIfNode.js
+++ runtime/render/updateIfNode.js
@@ -18,20 +18,12 @@ export default function updateIfNode(node, meta, Component) {
     if (bool) {
       if (Component) {
         component.update.call(this, node, meta, Component);
-        // const vm = vms.get(node.__vmid__);
-        // // TODO check props update
-        // const props = getProps.call(this, meta.props, Component.props);
-        // Object.assign(vm.$props, props);
-        // updateElement.call(vm, node, Component.meta);
       } else {
         updateElement.call(this, node, meta);
       }
     } else {
       if (Component) {
         component.remove(node);
-        // const vm = vms.get(node.__vmid__);
-        // Object.defineProperty(vm, '$destroy', {value: true});
-        // vms.delete(node.__vmid__);
       }
       node.parentNode.replaceChild(document.createComment(''), node);
     }
