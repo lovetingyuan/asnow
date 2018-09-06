3f906714b9031c1e3947cda73aa061c75bb0b06a
Baitu Huang <baitu.huang@shopeemobile.com>
2018-09-06 17:03:09 +0800

    fix:use parse5

--- compiler/node/index.js
+++
@@ -1,7 +0,0 @@
-import parseNode from './parseNode.js';
-
-export default function compile(template) {
-  const ast = parse5.parseFragment(template.trim());
-  const root = ast.childNodes[0];
-  return parseNode(root);
-}
--- compiler/node/parseExpression.js
+++
@@ -1,6 +0,0 @@
-export default function parseExpression(expression, isText) {
-  return {
-    expression,
-    vars: []
-  }
-}
